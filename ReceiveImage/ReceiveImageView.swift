5ec705f3f57a303c2e2245c2e515faa1b3503245
Lloyd Massiah <artmis9@protonmail.com>
2025-12-16 12:41:40 -0500

    format receive image extension code

--- ReceiveImage/ReceiveImageView.swift
+++ ReceiveImage/ReceiveImageView.swift
@@ -8,159 +8,159 @@
 import SwiftUI
 
 struct ReceiveImageView: View {
-    // let image: UIImage
-    let image: Data
-    let dismiss: () -> Void
-    @State var projects: [SharedProject] = []
-    @State var selection: Int64 = 0
-    @State var error = ""
-    @State var showError = false
+  // let image: UIImage
+  let image: Data
+  let dismiss: () -> Void
+  @State var projects: [SharedProject] = []
+  @State var selection: Int64 = 0
+  @State var error = ""
+  @State var showError = false
 
-    private var hasNoProject: Bool {
-        projects.isEmpty
+  private var hasNoProject: Bool {
+    projects.isEmpty
+  }
+
+  var body: some View {
+    VStack {
+      HStack {
+        Spacer()
+        Button {
+          dismiss()
+        } label: {
+          Image(systemName: "xmark.circle")
+            .font(.system(size: 28))
+            .fontWeight(.light)
+            .foregroundStyle(Color.black.opacity(0.8))
+        }
+        .padding(.trailing, 8)
+      }
+      if hasNoProject {
+        Text(
+          "Please create one project in the main app before trying to share."
+        )
+        if let sharedImage = UIImage(data: image) {
+          Image(uiImage: sharedImage)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+        } else {
+          Text("Couldn't load image")
+        }
+
+      } else {
+        Picker("Project", selection: $selection) {
+          ForEach(projects, id: \.self.id) { project in
+            Text(project.name)
+          }
+        }
+        .pickerStyle(.menu)
+        if let sharedImage = UIImage(data: image) {
+          Image(uiImage: sharedImage)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+        } else {
+          Text("Couldn't load image")
+        }
+
+        Button("Save to selected project") {
+          do {
+            try saveImageForProject(
+              projectId: selection,
+              image: image
+            )
+            dismiss()
+          } catch {
+            // TODO: handle error
+            self.error = "Couldn't save image. Please try again."
+            self.showError = true
+          }
+        }
+      }
     }
-
-    var body: some View {
-        VStack {
-            HStack {
-                Spacer()
-                Button {
-                    dismiss()
-                } label: {
-                    Image(systemName: "xmark.circle")
-                        .font(.system(size: 28))
-                        .fontWeight(.light)
-                        .foregroundStyle(Color.black.opacity(0.8))
-                }
-                .padding(.trailing, 8)
-            }
-            if hasNoProject {
-                Text(
-                    "Please create one project in the main app before trying to share."
-                )
-                if let sharedImage = UIImage(data: image) {
-                    Image(uiImage: sharedImage)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                } else {
-                    Text("Couldn't load image")
-                }
-
-            } else {
-                Picker("Project", selection: $selection) {
-                    ForEach(projects, id: \.self.id) { project in
-                        Text(project.name)
-                    }
-                }
-                .pickerStyle(.menu)
-                if let sharedImage = UIImage(data: image) {
-                    Image(uiImage: sharedImage)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                } else {
-                    Text("Couldn't load image")
-                }
-
-                Button("Save to selected project") {
-                    do {
-                        try saveImageForProject(
-                            projectId: selection,
-                            image: image
-                        )
-                        dismiss()
-                    } catch {
-                        // TODO: handle error
-                        self.error = "Couldn't save image. Please try again."
-                        self.showError = true
-                    }
-                }
-            }
-        }
-        .alert(
-            error,
-            isPresented: $showError
-        ) {
-            Button("Ok") {
-                self.error = ""
-                self.showError = false
-            }
-        }
-        .padding(.horizontal, 10)
-        .task {
-            do {
-                let projects = try getProjects()
-                self.projects = projects
-                if let first = projects.first {
-                    selection = first.id
-                }
-            } catch {
-                // TODO: show some kind of error and offer retry or something
-                print(error.localizedDescription)
-            }
-        }
+    .alert(
+      error,
+      isPresented: $showError
+    ) {
+      Button("Ok") {
+        self.error = ""
+        self.showError = false
+      }
     }
+    .padding(.horizontal, 10)
+    .task {
+      do {
+        let projects = try getProjects()
+        self.projects = projects
+        if let first = projects.first {
+          selection = first.id
+        }
+      } catch {
+        // TODO: show some kind of error and offer retry or something
+        print(error.localizedDescription)
+      }
+    }
+  }
 }
 
 func getProjects() throws -> [SharedProject] {
-    guard let data = try? SharedPersistence().getFile(fileName: "projects")
-    else {
-        // throw an error
-        throw ShareError.getFile(
-            "Couldn't load projects. Head to the main app and create a project"
-        )
-    }
-    let decoder = JSONDecoder()
-    guard let projects = try? decoder.decode([SharedProject].self, from: data)
-    else {
-        throw ShareError.emptyFile(
-            "Couldn't load projects. Head to the main app and create a project"
-        )
-    }
+  guard let data = try? SharedPersistence().getFile(fileName: "projects")
+  else {
+    // throw an error
+    throw ShareError.getFile(
+      "Couldn't load projects. Head to the main app and create a project"
+    )
+  }
+  let decoder = JSONDecoder()
+  guard let projects = try? decoder.decode([SharedProject].self, from: data)
+  else {
+    throw ShareError.emptyFile(
+      "Couldn't load projects. Head to the main app and create a project"
+    )
+  }
 
-    return projects
+  return projects
 }
 
 let sharedImagesFileName = "sharedImages"
 
 func saveImageForProject(projectId: Int64, image: Data) throws {
-    let fileIdentifier = UUID().uuidString
-    let sharedImageIdentification = SharedImage(
-        projectId: projectId,
-        fileIdentifier: fileIdentifier
-    )
+  let fileIdentifier = UUID().uuidString
+  let sharedImageIdentification = SharedImage(
+    projectId: projectId,
+    fileIdentifier: fileIdentifier
+  )
 
-    try SharedPersistence().saveImage(
-        fileIdentifier: sharedImageIdentification.fileIdentifier,
-        image: image
-    )
+  try SharedPersistence().saveImage(
+    fileIdentifier: sharedImageIdentification.fileIdentifier,
+    image: image
+  )
 
-    let fileData = try SharedPersistence().getFile(
-        fileName: sharedImagesFileName
-    )
+  let fileData = try SharedPersistence().getFile(
+    fileName: sharedImagesFileName
+  )
 
-    guard let data = fileData else {
-        let sharedImages = [sharedImageIdentification]
-        let encoder = JSONEncoder()
-        let updatedSharedImagesList = try encoder.encode(sharedImages)
-        try SharedPersistence().writeFile(
-            data: updatedSharedImagesList,
-            fileName: sharedImagesFileName
-        )
-        return
-    }
-
-    let decoder = JSONDecoder()
-    guard var sharedImages = try? decoder.decode([SharedImage].self, from: data)
-    else {
-        throw ShareError.emptyFile("Couldn't get shared images list file")
-    }
-
-    sharedImages.append(sharedImageIdentification)
-
+  guard let data = fileData else {
+    let sharedImages = [sharedImageIdentification]
     let encoder = JSONEncoder()
     let updatedSharedImagesList = try encoder.encode(sharedImages)
     try SharedPersistence().writeFile(
-        data: updatedSharedImagesList,
-        fileName: sharedImagesFileName
+      data: updatedSharedImagesList,
+      fileName: sharedImagesFileName
     )
+    return
+  }
+
+  let decoder = JSONDecoder()
+  guard var sharedImages = try? decoder.decode([SharedImage].self, from: data)
+  else {
+    throw ShareError.emptyFile("Couldn't get shared images list file")
+  }
+
+  sharedImages.append(sharedImageIdentification)
+
+  let encoder = JSONEncoder()
+  let updatedSharedImagesList = try encoder.encode(sharedImages)
+  try SharedPersistence().writeFile(
+    data: updatedSharedImagesList,
+    fileName: sharedImagesFileName
+  )
 }
--- ReceiveImage/ShareViewController.swift
+++ ReceiveImage/ShareViewController.swift
@@ -11,63 +11,68 @@ import UIKit
 import UniformTypeIdentifiers
 
 class ShareViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-        // Ensure access to extensionItem and itemProvider
-        guard
-            let extensionItem = extensionContext?.inputItems.first as? NSExtensionItem,
-            let itemProvider = extensionItem.attachments?.first
-        else {
-            // TODO: do some error handling
-            close()
-            return
+    // Ensure access to extensionItem and itemProvider
+    guard
+      let extensionItem = extensionContext?.inputItems.first as? NSExtensionItem,
+      let itemProvider = extensionItem.attachments?.first
+    else {
+      // TODO: do some error handling
+      close()
+      return
+    }
+    // Check type identifier
+    let imageDataType = UTType.data.identifier
+    if itemProvider.hasItemConformingToTypeIdentifier(imageDataType) {
+      itemProvider.loadItem(forTypeIdentifier: imageDataType, options: nil) {
+        providedImage,
+        error in
+        if let importError = error {
+          return
         }
-        // Check type identifier
-        let imageDataType = UTType.data.identifier
-        if itemProvider.hasItemConformingToTypeIdentifier(imageDataType) {
-            itemProvider.loadItem(forTypeIdentifier: imageDataType, options: nil) { providedImage, error in
-                if let importError = error {
-                    print(importError.localizedDescription)
-                    return
-                }
 
-                if let url = providedImage as? NSURL {
-                    do {
-                        let data = try Data(contentsOf: url as URL)
-                        
-                        func dismissView() {
-                            self.extensionContext?.completeRequest(returningItems: nil, completionHandler: nil)
-                        }
-                        
-                        DispatchQueue.main.async {
-                            // host the SwiftU view
-                            let contentView = UIHostingController(rootView: ReceiveImageView(image: data, dismiss: dismissView))
-                            self.addChild(contentView)
-                            self.view.addSubview(contentView.view)
-                            contentView.view.backgroundColor = UIColor.white
-                            contentView.view.isOpaque = true
+        if let url = providedImage as? NSURL {
+          do {
+            let data = try Data(contentsOf: url as URL)
 
-                            // set up constraints
-                            contentView.view.translatesAutoresizingMaskIntoConstraints = false
-                            contentView.view.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
-                            contentView.view.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
-                            contentView.view.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-                            contentView.view.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
-                        }
-                    } catch {
-                        // TODO: show an error view that says couldn't load image
-                    }
-                }
+            func dismissView() {
+              self.extensionContext?.completeRequest(returningItems: nil, completionHandler: nil)
             }
-        } else {
-            close()
-            return
-        }
-    }
 
-    /// Close the Share Extension
-    func close() {
-        extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
+            DispatchQueue.main.async {
+              // host the SwiftU view
+              let contentView = UIHostingController(
+                rootView: ReceiveImageView(image: data, dismiss: dismissView)
+              )
+              self.addChild(contentView)
+              self.view.addSubview(contentView.view)
+              contentView.view.backgroundColor = UIColor.white
+              contentView.view.isOpaque = true
+
+              // set up constraints
+              contentView.view.translatesAutoresizingMaskIntoConstraints = false
+              contentView.view.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+              contentView.view.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive =
+                true
+              contentView.view.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+              contentView.view.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive =
+                true
+            }
+          } catch {
+            // TODO: show an error view that says couldn't load image
+          }
+        }
+      }
+    } else {
+      close()
+      return
     }
+  }
+
+  /// Close the Share Extension
+  func close() {
+    extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
+  }
 }
