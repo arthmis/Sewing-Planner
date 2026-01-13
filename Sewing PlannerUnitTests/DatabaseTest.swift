cf32a0acea4f9b2cdfc5e8f04536c7ed941c2ff1
Lloyd Massiah <artmis9@protonmail.com>
2026-01-13 16:34:18 -0500

    add a project for getting a project by id

--- Sewing PlannerUnitTests/DatabaseTest.swift
+++ Sewing PlannerUnitTests/DatabaseTest.swift
@@ -12,6 +12,11 @@ import Testing
 @testable import Sewing_Planner
 
 struct Sewing_PlannerDatabaseTests {
+  private func seedProject(project: ProjectMetadataInput, db: AppDatabase) throws {
+    var project = project
+    _ = try db.addProject(project: &project)
+  }
+
   @Test("Test add project")
   func testAddProject() throws {
     let db = AppDatabase.empty()
@@ -28,5 +33,37 @@ struct Sewing_PlannerDatabaseTests {
 
     #expect(project.id == 1)
     #expect(project.name == "Project 1")
+  }
+
+  @Test("Test get project")
+  func testGetProject() throws {
+    let db = AppDatabase.empty()
+    let now = Date()
+    let projectInput = ProjectMetadataInput(
+      id: nil,
+      name: "Project 1",
+      completed: false,
+      createDate: now,
+      updateDate: now
+    )
+    try seedProject(project: projectInput, db: db)
+    let projectInput2 = ProjectMetadataInput(
+      id: nil,
+      name: "Project 2",
+      completed: true,
+      createDate: now,
+      updateDate: now
+    )
+    try seedProject(project: projectInput2, db: db)
+
+    var project = try db.getProject(id: 1)!
+
+    #expect(project.name == "Project 1")
+    #expect(project.completed == false)
+
+    project = try db.getProject(id: 2)!
+
+    #expect(project.name == "Project 2")
+    #expect(project.completed == true)
   }
 }
