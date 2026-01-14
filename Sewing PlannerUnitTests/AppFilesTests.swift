a31d60bbf26957133b7dc5037c632f1303984941
Lloyd Massiah <artmis9@protonmail.com>
2026-01-14 14:16:35 -0500

    add test for getting an image after storing it

--- Sewing PlannerUnitTests/AppFilesTests.swift
+++ Sewing PlannerUnitTests/AppFilesTests.swift
@@ -5,7 +5,10 @@ import UIKit
 @testable import Sewing_Planner
 
 struct Sewing_PlannerAppFilesTests {
-  func createSolidColorImage(size: CGSize, color: UIColor, scale: CGFloat = UIScreen.main.scale)
+  // normally might set the scale to the device but because devices have different scales
+  // it's better to manually set it to get consistent results
+  // this only matters when comparing input image with the image saved on disk
+  func createSolidColorImage(size: CGSize, color: UIColor, scale: CGFloat)
     -> UIImage
   {
     let format = UIGraphicsImageRendererFormat()
@@ -24,7 +27,7 @@ struct Sewing_PlannerAppFilesTests {
   func testSaveImage() throws {
     let size = CGSize(width: 200, height: 200)
     let color = UIColor.white
-    let image = createSolidColorImage(size: size, color: color)
+    let image = createSolidColorImage(size: size, color: color, scale: 1)
 
     let result = try AppFiles().saveProjectImage(
       projectId: 1,
@@ -36,5 +39,21 @@ struct Sewing_PlannerAppFilesTests {
     let filePath = AppFiles().getPathForImage(forProject: 1, fileIdentifier: result!.0)
     let exists = fileManager.fileExists(atPath: filePath.path)
     #expect(exists == true)
+  }
+
+  @Test("Test get image")
+  func testGetImage() throws {
+    let size = CGSize(width: 600, height: 600)
+    let color = UIColor.white
+    let image = createSolidColorImage(size: size, color: color, scale: 1)
+
+    let result = try AppFiles().saveProjectImage(
+      projectId: 1,
+      image: ProjectImageInput(image: image)
+    )
+
+    let storedImage = AppFiles().getImage(for: result!.0, fromProject: 1)
+    #expect(storedImage != nil)
+    #expect(storedImage!.pngData() == image.pngData())
   }
 }
